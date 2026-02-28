4e3cc88fd76acdd6d11d544d071093ed0f3ba4db
jazmirafy <egyptjazmean@gmail.com>
2026-02-28 13:34:14 -0600

    created unity project

---
+++ Dialogue System/Assets/Scripts/DialogueDatabase.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueDatabase", menuName = "Scriptable Objects/DialogueDatabase")]
+public class DialogueDatabase : ScriptableObject
+{
+    
+}
